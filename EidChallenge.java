7cf257889e55ed7988a856386f8e7ef49e483e7c
Shoaib-Mohammedd <shuaibmuhammed149@gmail.com>
2022-12-02 12:57:40 +0300

    Eid Chalenge

---
+++ EidChallenge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EidChallenge {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println( " Enter A Number To Convert Decimal to HexDecimal ");
+        long DecimalToHexdecimal = input.nextInt();
+        System.out.println( " The Decimal = " + DecimalToHexdecimal );
+
+
+        System.out.println( 35 * 10 % 56 );
+    }
+}
